76292f5a4d8740d0ab5606eea63a952366b93ed5
sebax401 <cabrerasebastian867@gmail.com>
2026-06-05 20:49:36 -0400

    Crea Base de datos SQLite Automaticamente

--- ApiTransporte/Program.cs
+++ ApiTransporte/Program.cs
@@ -1,6 +1,8 @@
 using ApiTransporte.Data;
 using Microsoft.EntityFrameworkCore;
 
+using ApiTransporte.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -21,6 +23,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
 
 app.UseSwagger();
 app.UseSwaggerUI();
