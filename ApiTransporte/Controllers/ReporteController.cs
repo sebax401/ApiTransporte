49fcf637daae4f2d2efb7a30bccff4ef107a0d36
sebax401 <cabrerasebastian867@gmail.com>
2026-06-06 19:32:33 -0400

    ASctualizar Reporte

---
+++ ApiTransporte/Controllers/ReporteController.cs
@@ -0,0 +1,38 @@
+﻿using ApiTransporte.Data;
+using ApiTransporte.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiTransporte.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ReportesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("vehiculo/{idVehiculo}")]
+        public async Task<ActionResult<IEnumerable<Reporte>>> ObtenerReportesVehiculo(int idVehiculo)
+        {
+            return await _context.Reporte
+                .Where(r => r.IdVehiculo == idVehiculo)
+                .ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Reporte>> CrearReporte(Reporte reporte)
+        {
+            reporte.FechaGeneracion = DateTime.Now;
+
+            _context.Reporte.Add(reporte);
+            await _context.SaveChangesAsync();
+
+            return Ok(reporte);
+        }
+    }
+}
