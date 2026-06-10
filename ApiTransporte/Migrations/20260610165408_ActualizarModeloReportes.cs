923f525e6dc6c9f9ddcb391b64a01fc81bc51f30
sebax401 <cabrerasebastian867@gmail.com>
2026-06-10 12:55:22 -0400

    Migrar Base de datos

---
+++ ApiTransporte/Migrations/20260610165408_ActualizarModeloReportes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiTransporte.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualizarModeloReportes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LugarCompra",
+                table: "Reporte",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ValorRepuesto",
+                table: "Reporte",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LugarCompra",
+                table: "Reporte");
+
+            migrationBuilder.DropColumn(
+                name: "ValorRepuesto",
+                table: "Reporte");
+        }
+    }
+}
