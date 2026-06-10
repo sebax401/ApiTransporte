595a81048721ddd3115a7bcb4d808908a2f113c8
sebax401 <cabrerasebastian867@gmail.com>
2026-06-10 13:49:13 -0400

    Arreglos de bugs

--- ApiTransporte/Program.cs
+++ ApiTransporte/Program.cs
@@ -16,6 +16,10 @@ builder.Services.AddCors(options =>
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
